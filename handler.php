b2f54dd0a4e50c77ef5aeedb9b14b61650d07ea9
MegaScrew <MegaScrew@mail.ru>
2021-09-16 09:53:05 +0300

    added case 5

--- handler.php
+++ handler.php
@@ -103,6 +103,22 @@ switch ($_POST['Step']) {
 		// 	print_r($temp);
 		// echo '</pre>';
 		break;
+	case '5':
+		header('Content-Type: text/csv');
+		header('Content-Disposition: attachment; filename = '.$_POST['name'].' c '.$_POST['first_day'].'po'.$_POST['last_day'].'.csv');
+
+		$output = fopen("php://output", "w");
+
+		$pattern = $_POST['pattern'];
+		$name = $_POST['name'];
+		$name = $name.'_data';
+		$data = json_decode($_POST['data'], true);
+
+		getCSV($data, $name, $output, $pattern);
+
+		fclose($output);
+
+		break;
 	default:
 		echo '<pre>';
 			echo 'default';
